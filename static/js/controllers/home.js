c762ffcdca559b8857ebfc929c7e4e3d7373fb92
gustavogmrs <gustavoguimaraes18@gmail.com>
2015-05-09 18:40:59 -0300

    Atualizando

--- static/js/controllers/home.js
+++ static/js/controllers/home.js
@@ -1,23 +1,5 @@
 angular.module('MagicApp',['ui.bootstrap','mrhttp','webcam']);
 
-angular.module('MagicApp').controller('ModalCtrl', function ($scope, $modal) {
-
-  $scope.open = function () {
-    var modalInstance = $modal.open();
-  };
-});
-
-angular.module('MagicApp').controller('ModalInstanceCtrl', function ($scope, $modalInstance) {
-
-  $scope.ok = function () {
-    $modalInstance.close();
-  };
-
-  $scope.cancel = function () {
-    $modalInstance.dismiss('cancel');
-  };
-});
-
 angular.module('MagicApp').controller("CameraCtrl", function ($scope) {
 	$scope.cameraVisible = false;
 	$scope.mostrarCamera = function() {
--- static/js/controllers/maps.js
+++ static/js/controllers/maps.js
@@ -1,27 +1,69 @@
 // ---------   MAP  --------------
 
-angular.module('MagicApp').controller('MainCtrl', function ($scope, $interval, $modal, MRHttp) {
+angular.module('MagicApp').controller('MainCtrl', function ($scope, $interval, MRHttp) {
 
     $scope.mapaVisible = true;
+    $scope.conteudoLayer = false;
+    $scope.formVisible = false;
     $scope.cameraVisible = false;
-    $scope.alternarMapaCamera = function() {
-        $scope.mapaVisible = !$scope.mapaVisible;
-        $scope.cameraVisible = !$scope.cameraVisible;
+
+    $scope.btnCriar = true;
+    $scope.btnCancelar = false;
+    $scope.btnAbrir = false;
+    $scope.btnSair = false;
+    $scope.btnFoto = false;
+
+    $scope.mostrarForm = function() {
+        $scope.mapaVisible = false;
+        $scope.formVisible = true;
+        $scope.btnCriar = false;
+        $scope.btnCancelar = true;
+        $scope.latitude = userPosition.A;
+        $scope.longitude = userPosition.F;
+    }
+
+    $scope.mostrarMapa = function() {
+        $scope.mapaVisible = true;
+        $scope.formVisible = false;
+        $scope.btnCriar = true;
+        $scope.btnCancelar = false;
+    }
+
+    $scope.mostrarLayer = function() {
+        $scope.mapaVisible = false;
+        $scope.conteudoLayer = true;
+        $scope.btnAbrir = false;
+        $scope.btnSair = true;
+    }
+
+    $scope.sairLayer = function() {
+        $scope.btnSair = false;
+        $scope.btnAbrir = true;
+        $scope.conteudoLayer = false;
+        $scope.mapaVisible = true;
+    }
+
+    $scope.abrirCamera = function() {
+        $scope.conteudoLayer = false;
+        $scope.cameraVisible = true;
+        $scope.btnSair = false;
+        $scope.btnFoto = true;
     }
 
     //var userLat = -23.199385; // Santos Dumont Lat
     //var userLng = -45.891001; // Santos Dumont Lng
-    var userLat = -23.198069; // Vicentina Aranha 2 Lat
-    var userLng = -45.896236; // Vicentina Aranha 2 Lng
+    //var userLat = -23.198069; // Vicentina Aranha 2 Lat
+    //var userLng = -45.896236; // Vicentina Aranha 2 Lng
     //var userLat = -23.198300; // Vicentina Aranha Lat
     //var userLng = -45.894200; // Vicentina Aranha Lng
-    var userPosition = new google.maps.LatLng(userLat, userLng);
-    var layerPosition = new google.maps.LatLng(-23.198169, -45.895000);
+    //var userLat = -23.160903; // FATEC
+    //var userLng = -45.795815; // FATEC
+    var userPosition = null;
 
     var mapOptions = {
         center: userPosition,
         disableDefaultUI: true,
-        zoom: 17
+        zoom: 18
     }
     var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
@@ -38,7 +80,7 @@ angular.module('MagicApp').controller('MainCtrl', function ($scope, $interval, $
         criarLayers();
     })
     .error(function(result){
-
+        console.log('Erro ao buscar Layers');
     });
 
     $scope.imgs;
@@ -53,36 +95,33 @@ angular.module('MagicApp').controller('MainCtrl', function ($scope, $interval, $
             $scope.imgs = result.images;
         })
         .error(function(result){
+            console.log('Erro ao buscar imagens');
         });        
     }
-    
-
 
     var layerOptions = {
-            strokeColor: '#FF0000',
+            strokeColor: '#1000FF',
             strokeOpacity: 0.8,
             strokeWeight: 2,
-            fillColor: '#FF0000',
+            fillColor: '#1000FF',
             fillOpacity: 0.35,
             map: map,
-            center: layerPosition,
-            radius: 30
+            center: null,
+            radius: null
             }; 
-    var criarLayers = function(){
+
+    criarLayers = function(){
         for(var i=0; i<layers.length; ++i){
-            layers[i]
             layerOptions.center = new google.maps.LatLng(layers[i].latitude,layers[i].longitude);
             layerOptions.radius = layers[i].radius;
             var circle = new google.maps.Circle(layerOptions);
 
-
             var infowindowLayer = new google.maps.InfoWindow({
-            map: map,
-            position: new google.maps.LatLng(layers[i].latitude,layers[i].longitude),
-            content: layers[i].name
+                map: map,
+                position: layerOptions.center,
+                content: layers[i].name
             });
-
-
+            console.log('Criando layers');
         }     
     }
 
@@ -90,39 +129,58 @@ angular.module('MagicApp').controller('MainCtrl', function ($scope, $interval, $
         return (google.maps.geometry.spherical.computeDistanceBetween(p1, p2)).toFixed(2);
     }
 
-    //var timer = $interval(autoUpdate,100); //Timer para simular usuário andando no mapa
+     var timer = $interval(autoUpdate,1000);
 
     function autoUpdate() {
-        userPosition = new google.maps.LatLng(userLat, userLng);
-        marker.setPosition(userPosition);
-        map.setCenter(userPosition);
+        
+        navigator.geolocation.getCurrentPosition(
+                displayPosition, 
+                displayError
+            );
 
-        for(var i=0; i<layers.length; ++i){
-            layerPosition = new google.maps.LatLng(layers[i].latitude, layers[i].longitude);
-            if(calcDistance(userPosition, layerPosition) < layers[i].radius){
-                $interval.cancel(timer); // stop timer
-                getImages(layers[i]);
-                $scope.descricao = " Layer: "+layers[i].name;
-                $scope.escondeMapa = false;
-                /*
-                $modal.open({
-                    templateUrl: 'myModalContent.html',
-                    controller: 'ModalInstanceCtrl'
-                });
-                */
+        function displayPosition(position) {
+            userPosition = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            marker.setPosition(userPosition);
+            map.setCenter(userPosition);
+
+            for(var i=0; i<layers.length; ++i){
+                layerPosition = new google.maps.LatLng(layers[i].latitude, layers[i].longitude);
+                if(calcDistance(userPosition, layerPosition) < layers[i].radius){
+                    $interval.cancel(timer); // stop timer
+                    getImages(layers[i]);
+                    $scope.descricao = layers[i].name;
+                    $scope.btnAbrir = true;
+                    $scope.btnCriar = false;            }
             }
         }
-        userLng -= 0.000010;
+        function displayError(error) {
+            var errors = { 
+                1: 'Permission denied',
+                2: 'Position unavailable',
+                3: 'Request timeout'
+            };
+            alert("Error: " + errors[error.code]);
+        }
     }
 
+    $scope.myChannel = {
+        // the fields below are all optional
+        videoWidth: 600,
+        video: null // Will reference the video element on success
+      };
 
-
-$scope.myChannel = {
-    // the fields below are all optional
-    videoHeight: 380,
-    videoWidth: 290,
-    video: null // Will reference the video element on success
-  };
+    $scope.submitLayer = function() {
+        $scope.form["latitude"] = userPosition.A;
+        $scope.form["longitude"] = userPosition.F;
+        MRHttp.post('http://magicsurfacebr.appspot.com/layer/save', $scope.form)
+        .success(function(result){
+            $scope.mostrarMapa();
+            criarLayers();
+        })
+        .error(function(result){
+            $scope.mensagem = "Erro ao enviar formulário.";
+        });  
+    }
 
 
 });
