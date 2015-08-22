c9f669559d40ae437e63d183e0b2571fc452c68e
gustavo-guimaraes <gustavo.guimaraes18@gmail.com>
2015-08-22 17:12:42 -0300

    Removento arquivos

--- static/js/controllers/home.js
+++
@@ -1,8 +0,0 @@
-angular.module('MagicApp',['ui.bootstrap','mrhttp','webcam']);
-
-angular.module('MagicApp').controller("CameraCtrl", function ($scope) {
-	$scope.cameraVisible = false;
-	$scope.mostrarCamera = function() {
-		$scope.cameraVisible = true;
-	}
-});
